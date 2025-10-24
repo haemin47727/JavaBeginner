a48722d29a0b73575bff186aa95ce456c08e0ac3
haemin47727 <sginstockx@gmail.com>
2025-10-24 10:23:02 -0400

    mysql file

--- src/main/java/db/MySQLConnect.java
+++ src/main/java/db/MySQLConnect.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQLConnect {
-    private static final String URL = "jdbc:mysql://localhost:3306/couples?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://your-localhost/couples?useSSL=false&serverTimezone=UTC";
     private static final String USER = "your-username";
     private static final String PASSWORD = "your-password";
 
@@ -13,7 +13,7 @@ public class MySQLConnect {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // ✅ Add this line
             return DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/couples?useSSL=false&serverTimezone=UTC",
+                    "jdbc:mysql://your-localhost/couples?useSSL=false&serverTimezone=UTC",
                     "your-username",
                     "your-password"
             );
